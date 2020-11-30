a26767908c3ab5a2d9cbe8f521a8d3c4a8f11c36
Mohamad Abdallah <mohamad1031998@gmail.com>
2020-11-30 20:10:54 +0100

    task 3

---
+++ crypto/ec_from_pub.c
@@ -0,0 +1,42 @@
+#include "hblk_crypto.h"
+
+/**
+ * ec_from_pub - a function that creates an EC_KEY structure given a public key
+ * @pub: the public key to be converted
+ * containing both the public and private keys
+ * or NULL upon failure
+ *
+ * Return: a pointer to the created EC_KEY structure, or NULL upon failure
+ */
+EC_KEY *ec_from_pub(uint8_t const pub[EC_PUB_LEN])
+{
+	EC_POINT *pub_key;
+	EC_GROUP *group;
+	EC_KEY *ec_key;
+
+	if (pub == NULL)
+		return (NULL);
+	ec_key = EC_KEY_new_by_curve_name(EC_CURVE);
+	if (ec_key == NULL)
+		return (NULL);
+	group = (EC_GROUP *)EC_KEY_get0_group(ec_key);
+	pub_key = EC_POINT_new(group);
+	if (pub_key == NULL)
+	{
+		EC_KEY_free(ec_key);
+		return (NULL);
+	}
+	if (!EC_POINT_oct2point(group, pub_key, pub, EC_PUB_LEN, NULL))
+	{
+		EC_KEY_free(ec_key);
+		EC_POINT_free(pub_key);
+	}
+	if (!EC_KEY_set_public_key(ec_key, pub_key))
+	{
+		EC_KEY_free(ec_key);
+		EC_POINT_free(pub_key);
+	}
+
+	EC_POINT_free(pub_key);
+	return (ec_key);
+}
