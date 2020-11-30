de4b9c313917fcfc52bb8d7bf20e38a7ed598fa4
Mohamad Abdallah <mohamad1031998@gmail.com>
2020-11-30 18:45:37 +0100

    task 0

--- crypto/sha256.c
+++ crypto/sha256.c
@@ -8,7 +8,8 @@
  *
  * Return: a pointer to digest or NULL if digest happens to be NULL
  */
-uint8_t *sha256(int8_t const *s, size_t len, uint8_t digest[SHA256_DIGEST_LENGTH])
+uint8_t *sha256(int8_t const *s, size_t len,
+		uint8_t digest[SHA256_DIGEST_LENGTH])
 {
 	if (digest == NULL)
 		return (NULL);
