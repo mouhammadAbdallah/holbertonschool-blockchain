160bde377f4d352ddf4d5c1c598cbd7c975c9b79
Mohamad Abdallah <mohamad1031998@gmail.com>
2020-12-09 01:20:41 +0100

    task 6

--- blockchain/v0.1/blockchain_deserialize.c
+++ blockchain/v0.1/blockchain_deserialize.c
@@ -1,4 +1,8 @@
 #include "blockchain.h"
+
+#define CLEAN_UP (free(chain), close(fd))
+#define CLEAN_UP_BLOCKS (free(block), llist_destroy(list, 1, NULL))
+#define CHECK_ENDIAN(x) (endianness ? SWAPENDIAN(x) : (void)0)
 /**
  * blockchain_deserialize - deserializes blockchain from file
  * @path: path to serialized blockchain file
@@ -19,47 +23,23 @@ blockchain_t *blockchain_deserialize(char const *path)
 		return (NULL);
 	if (read(fd, buf, strlen(HBLK_MAGIC)) != strlen(HBLK_MAGIC) ||
 	    strcmp(buf, HBLK_MAGIC))
-	{
-		free(chain);
-		close(fd);
-		return (NULL);
-	}
+		return (CLEAN_UP, NULL);
 	buf[strlen(HBLK_VERSION)] = 0;
 	if (read(fd, buf, strlen(HBLK_VERSION)) != strlen(HBLK_VERSION) ||
 	    strcmp(buf, HBLK_VERSION))
-	{
-		free(chain);
-		close(fd);
-		return (NULL);
-	}
+		return (CLEAN_UP, NULL);
 	chain = calloc(1, sizeof(*chain));
 	if (!chain)
-	{
-		free(chain);
-		close(fd);
-		return (NULL);
-	}
+		return (CLEAN_UP, NULL);
 	if (read(fd, &endianness, 1) != 1)
-	{
-		free(chain);
-		close(fd);
-		return (NULL);
-	}
+		return (CLEAN_UP, NULL);
 	endianness = endianness != _get_endianness();
 	if (read(fd, &size, 4) != 4)
-	{
-		free(chain);
-		close(fd);
-		return (NULL);
-	}
+		return (CLEAN_UP, NULL);
 	CHECK_ENDIAN(size);
 	chain->chain = deserialize_blocks(fd, size, endianness);
 	if (!chain)
-	{
-		free(chain);
-		close(fd);
-		return (NULL);
-	}
+		return (CLEAN_UP, NULL);
 	return (close(fd), chain);
 }
 
@@ -82,47 +62,23 @@ llist_t *deserialize_blocks(int fd, uint32_t size, uint8_t endianness)
 	{
 		block = calloc(1, sizeof(*block));
 		if (!block)
-		{
-			free(block);
-			llist_destroy(list, 1, NULL);
-			return (NULL);
-		}
+			return (CLEAN_UP_BLOCKS, NULL);
 		if (read(fd, &(block->info), sizeof(block->info)) != sizeof(block->info))
-		{
-			free(block);
-			llist_destroy(list, 1, NULL);
-			return (NULL);
-		}
+			return (CLEAN_UP_BLOCKS, NULL);
 		CHECK_ENDIAN(block->info.index);
 		CHECK_ENDIAN(block->info.difficulty);
 		CHECK_ENDIAN(block->info.timestamp);
 		CHECK_ENDIAN(block->info.nonce);
 		if (read(fd, &(block->data.len), 4) != 4)
-		{
-			free(block);
-			llist_destroy(list, 1, NULL);
-			return (NULL);
-		}
+			return (CLEAN_UP_BLOCKS, NULL);
 		CHECK_ENDIAN(block->data.len);
 		if (read(fd, block->data.buffer, block->data.len) != block->data.len)
-		{
-			free(block);
-			llist_destroy(list, 1, NULL);
-			return (NULL);
-		}
+			return (CLEAN_UP_BLOCKS, NULL);
 		if (read(fd, block->hash, SHA256_DIGEST_LENGTH) !=
 		    SHA256_DIGEST_LENGTH)
-		{
-			free(block);
-			llist_destroy(list, 1, NULL);
-			return (NULL);
-		}
+			return (CLEAN_UP_BLOCKS, NULL);
 		if (llist_add_node(list, block, ADD_NODE_REAR))
-		{
-			free(block);
-			llist_destroy(list, 1, NULL);
-			return (NULL);
-		}
+			return (CLEAN_UP_BLOCKS, NULL);
 	}
 	return (list);
 }
