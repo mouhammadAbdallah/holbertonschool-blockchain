0735612e846aef0e1d1912dfdbf0eb2d1212fc99
Mohamad Abdallah <mohamad1031998@gmail.com>
2020-12-20 14:28:32 +0100

    task 2

--- blockchain/v0.2/block_is_valid.c
+++ blockchain/v0.2/block_is_valid.c
@@ -1,20 +1,23 @@
 #include "blockchain.h"
 
-#define GENESIS_BLOCK { \
-	{ /* info */ \
-		0 /* index */, \
-		0, /* difficulty */ \
-		1537578000, /* timestamp */ \
-		0, /* nonce */ \
-		{0} /* prev_hash */ \
-	}, \
-	{ /* data */ \
-		"Holberton School", /* buffer */ \
-		16 /* len */ \
-	}, \
-	"\xc5\x2c\x26\xc8\xb5\x46\x16\x39\x63\x5d\x8e\xdf\x2a\x97\xd4\x8d" \
-	"\x0c\x8e\x00\x09\xc8\x17\xf2\xb1\xd3\xd7\xff\x2f\x04\x51\x58\x03" \
-}
+#define GENESIS_BLOCK                                                                  \
+	{                                                                              \
+		{                                                                      \
+		    /* info */                                                         \
+		    0 /* index */,                                                     \
+		    0,		/* difficulty */                                       \
+		    1537578000, /* timestamp */                                        \
+		    0,		/* nonce */                                            \
+		    {0}		/* prev_hash */                                        \
+		},                                                                     \
+		    {                                                                  \
+			/* data */                                                     \
+			"Holberton School", /* buffer */                               \
+			16		    /* len */                                  \
+		    },                                                                 \
+		    "\xc5\x2c\x26\xc8\xb5\x46\x16\x39\x63\x5d\x8e\xdf\x2a\x97\xd4\x8d" \
+		    "\x0c\x8e\x00\x09\xc8\x17\xf2\xb1\xd3\xd7\xff\x2f\x04\x51\x58\x03" \
+	}
 
 /**
  * block_is_valid - checks if this and previous block are valid
@@ -36,12 +39,15 @@ int block_is_valid(block_t const *block, block_t const *prev_block)
 	if (!block_hash(prev_block, hash_buf) ||
 	    memcmp(hash_buf, prev_block->hash, SHA256_DIGEST_LENGTH))
 		return (1);
-	if (memcmp(prev_block->hash, block->info.prev_hash, SHA256_DIGEST_LENGTH))
+	if (memcmp(prev_block->hash, block->info.prev_hash,
+		   SHA256_DIGEST_LENGTH))
 		return (1);
 	if (!block_hash(block, hash_buf) ||
 	    memcmp(hash_buf, block->hash, SHA256_DIGEST_LENGTH))
 		return (1);
 	if (block->data.len > BLOCKCHAIN_DATA_MAX)
+		return (1);
+	if (hash_matches_difficulty(block->hash, block->info.difficulty) == 0)
 		return (1);
 	return (0);
 }
