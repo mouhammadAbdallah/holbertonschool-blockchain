2281abe9a68e3836acbda92a32673aaa529e491f
Mohamad Abdallah <mohamad1031998@gmail.com>
2020-12-23 16:31:48 +0100

    task 3

--- blockchain/v0.2/blockchain_difficulty.c
+++ blockchain/v0.2/blockchain_difficulty.c
@@ -12,16 +12,16 @@ uint32_t blockchain_difficulty(blockchain_t const *blockchain)
 	block_t *block, *lAblock;
 	uint64_t expTime, actTime;
 
-	block = (block_t *)llist_get_tail(blockchain->chain);
+	block = llist_get_tail(blockchain->chain);
 	if (block->info.index % DIFFICULTY_ADJUSTMENT_INTERVAL == 0 &&
 	    block->info.index != 0)
 	{
 
 		lAblock = (block_t *)llist_get_node_at(blockchain->chain,
 						       llist_size(blockchain->chain) -
-							   DIFFICULTY_ADJUSTMENT_INTERVAL);
+							   DIFFICULTY_ADJUSTMENT_INTERVAL + 1);
 		expTime = DIFFICULTY_ADJUSTMENT_INTERVAL * BLOCK_GENERATION_INTERVAL;
-		actTime = time(NULL) - lAblock->info.timestamp;
+		actTime = block->info.timestamp - lAblock->info.timestamp;
 		if (actTime < expTime / 2)
 			return (block->info.difficulty + 1);
 		if (actTime > expTime * 2)
