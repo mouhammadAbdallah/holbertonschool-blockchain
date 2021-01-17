58b0cc7f7b7b17045db63217655480a1f7d68130
Mohamad Abdallah <mohamad1031998@gmail.com>
2021-01-17 20:23:31 +0100

    task 9

---
+++ blockchain/v0.3/transaction/transaction_destroy.c
@@ -0,0 +1,16 @@
+#include "transaction.h"
+
+/**
+ * transaction_destroy - frees tx structure
+ * @transaction: tx struct to free
+ * 
+ * Void
+ */
+void transaction_destroy(transaction_t *transaction)
+{
+	if (!transaction)
+		return;
+	llist_destroy(transaction->inputs, 1, free);
+	llist_destroy(transaction->outputs, 1, free);
+	free(transaction);
+}
