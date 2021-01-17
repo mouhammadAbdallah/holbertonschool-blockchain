bb864cdda25599e86ca93a6f3338836ec09d3af7
Mohamad Abdallah <mohamad1031998@gmail.com>
2021-01-17 18:45:49 +0100

    task 0

--- blockchain/v0.3/transaction/tx_out_create.c
+++ blockchain/v0.3/transaction/tx_out_create.c
@@ -17,7 +17,9 @@ tx_out_t *tx_out_create(uint32_t amount, uint8_t const pub[EC_PUB_LEN])
 	if (tx_out == NULL)
 		return (NULL);
 	tx_out->amount = amount;
-	memcpy(tx_out->pub, pub, EC_PUB_LEN);
-	sha256((int8_t const *)&(tx_out->amount), 4 + EC_PUB_LEN, tx_out->hash);
+	memcpy(tx_out->pub, pub, sizeof(tx_out->pub));
+	if (!sha256((int8_t const *)tx_out,
+		    sizeof(tx_out->amount) + sizeof(tx_out->pub), tx_out->hash))
+		return (free(tx_out), NULL);
 	return (tx_out);
 }
